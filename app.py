95b71055c75751511c09d8c5fafb0a9d9c019bca
metapropage <dianzia.adm@gmail.com>
2024-06-30 08:25:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.markdown(
+components.html(
     """
+    <h1>Streamlit Example</h1>
+    <p>This is an example of embedding HTML in Streamlit.</p>
     <a href="https://streamlit.io" target="_blank">Visit Streamlit</a>
     """,
-    unsafe_allow_html=True
+    height=300,
 )
